14fab31baab7c02226e6a43698db304f8c9bde5e
eran <eranka12@gmail.com>
2025-01-30 10:34:26 +0200

    changed cost categories to enum.

--- models/cost_model.js
+++ models/cost_model.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
 
 const costSchema = new mongoose.Schema({
-    description: { type: String, required: true },
-    category: { type: String, required: true },
-    userid: { type: String, required: true, ref: 'User' },
-    sum: { type: Number, required: true },
-    created_at: { type: Date, default: Date.now },
+    description: {type: String, required: true},
+    category: {
+        type: String,
+        required: true,
+        enum: ['food', 'health', 'housing', 'sport', 'education'],
+    },
+    userid: {type: String, required: true, ref: 'User'},
+    sum: {type: Number, required: true},
+    created_at: {type: Date, default: Date.now},
 });
 
 module.exports = mongoose.model('Cost', costSchema);
